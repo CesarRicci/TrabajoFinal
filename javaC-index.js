4b9beb6ab4f7e2f8762db0c4017b4790a04f03b6
CesarRicci <cesarricci7@gmail.com>
2022-10-03 17:22:28 -0300

    TrabajoFinal

--- javaC-index.js
+++ javaC-index.js
@@ -31,23 +31,3 @@ if(Number(edad) == edad){
 
 
 
-var bPreguntar = true;
-     
-        window.onbeforeunload = preguntarAntesDeSalir;
-     
-        function preguntarAntesDeSalir () {
-            var respuesta;
-     
-            if ( bPreguntar ) {
-                respuesta = confirm ( '¿Seguro que quieres salir?' );
-     
-                if ( respuesta ) {
-                    window.onunload = function () {
-                        return true;
-                    }
-                } else {
-                    return false;
-                }
-            }
-            
-        }
--- quienes somos/quienes-somos.js
+++ quienes somos/quienes-somos.js
@@ -16,24 +16,3 @@ function ocultar_menu(){
 }
 
 
-
-var bPreguntar = true;
-     
-        window.onbeforeunload = preguntarAntesDeSalir;
-     
-        function preguntarAntesDeSalir () {
-            var respuesta;
-     
-            if ( bPreguntar ) {
-                respuesta = confirm ( '¿Seguro que quieres salir?' );
-     
-                if ( respuesta ) {
-                    window.onunload = function () {
-                        return true;
-                    }
-                } else {
-                    return false;
-                }
-            }
-            
-        }
